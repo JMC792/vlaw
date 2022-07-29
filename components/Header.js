c30bbb63b838328b0cb0d79023f9703d1bbbe2d8
Mauricio Chavez <mauriciochavez792@gmail.com>
2022-07-29 16:08:29 -0400

    reverte changes to original

--- components/Header.js
+++ components/Header.js
@@ -15,12 +15,12 @@ function Header() {
           <div class="p-12  text-shadow">
             <div class="font-sans text-left pt-[200px] lg:pt-[100px]">
               {/*============= Row 1 / Title============= */}
-              <h2 class="lg:leading-[100px] text-[30px] md:text-[60px] leading-[1.2] text-yellow-700 font-serif uppercase">
+              <h2 class="lg:leading-[100px] text-[30px] md:text-[60px] leading-[1.2] text-slate-700 font-serif uppercase">
                 Valentin Law LLC
               </h2>
 
               {/*============= Row 2 / Subtitle ============= */}
-              <h3 class="text-[14px] md:text-[25px] mt-[12px] text-yellow-700 font-serif uppercase">
+              <h3 class="text-[14px] md:text-[25px] mt-[12px] text-slate-700 font-serif uppercase">
                 "Immigration for All"
               </h3>
 
