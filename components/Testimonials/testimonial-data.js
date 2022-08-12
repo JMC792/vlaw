6c50a61a3bb2f29ca24faf6818967d440f4e4a2a
Mauricio Chavez <mauriciochavez792@gmail.com>
2022-08-12 18:39:46 -0400

    converted testimonials from hard code to json

---
+++ components/Testimonials/testimonial-data.js
@@ -0,0 +1,58 @@
+export const testimonialData = [
+    {
+        title: 'BEST IMMIGRATION LAWYER IN ATLANTA GA',
+        paragraph: 'Julio was great ! Handle our case very well and was very patient with us ! I would love to post our picture on here but I can’t! Julio, continue to be the best lawyer you can be and bless someone else like you bless My husband and I',
+        name: 'Joseph',
+        date: 'May 31, 2022'
+    },
+    {
+        title: 'He’s great',
+        paragraph: 'I cannot thank Julio enough for all of his help over the\n' +
+            '                            years. I was blown away by his integrity and all the hard work\n' +
+            '                            he did for my family. His work was excellent and he always\n' +
+            '                            responded promptly and paid attention to detail. I would\n' +
+            '                            recommend Julio to anyone who is serious about having an\n' +
+            '                            experienced lawyer that they can trust. Will definitely work\n' +
+            '                            with him again in the future!',
+        name: 'Aakash',
+        date: 'May 15, 2022'
+    },
+    {
+        title: 'Helped my mother.5.0 stars',
+        paragraph: 'Finally after a year fighting our immigration court case.\n' +
+            '                            Julio was able to do the impossible with his impeccable\n' +
+            '                            argumentative skills and won. Very glad we hired him for our\n' +
+            '                            case!',
+        name: 'Franklin',
+        date: 'May 12, 2022'
+    },
+    {
+        title: 'Caring Lawyer',
+        paragraph: 'One thing I truly loved was that Julio listened to me and was\n' +
+            '                            personable in my conversation with him. He offers personal\n' +
+            '                            care that many big law firms don’t. Thank you Julio for caring\n' +
+            '                            about me as a person and for taking delicate care of my\n' +
+            '                            case.Finally after a year fighting our immigration court case.',
+        name: 'Anonymous',
+        date: 'July 12, 2022'
+    },
+    {
+        title: 'Thanks from the bottom of my heart',
+        paragraph: 'Julio is an amazing lawyer who take my nephew Michael and his\n' +
+            '                            family case with an amazing dedication ; he always being very\n' +
+            '                            profesional and very dedicated and courteous . Highly\n' +
+            '                            recommended . Thank you so much Julio to make my family happy',
+        name: 'Blanca',
+        date: 'April 27, 2022'
+    },
+    {
+        title: 'Incredible, just incredible',
+        paragraph: 'Julio is an incredibly talented lawyer. He handled my case in\n' +
+            '                            immigration court with extreme diligence, poise, and tact.\n' +
+            '                            Ultimately, he saved my family and I from being removed from\n' +
+            '                            the country to our home country- where we would inevitably\n' +
+            '                            live in fear. Thank you, just thank you',
+        name: 'Stephen',
+        date: 'April 27, 2022'
+    },
+]
