1482955d84323da7e2e6f199bcd4a122defbccb3
Mauricio Chavez <mauriciochavez792@gmail.com>
2022-09-07 18:43:24 -0400

    reverted back to old hero format

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -8,7 +8,9 @@ function Hero() {
       {/*########################### Hero ###########################*/}
       <header className="bg-back-1 bg-cover bg-no-repeat h-screen" id="header">
         {/*============= Container ============= */}
-        <div className="container mx-auto grid">
+        <div className="container mx-auto grid lg:grid-cols-2">
+
+            <div className=""></div>
 
           {/*============= Hero-Column-2 ============= */}
           <HeroText/>
--- components/Hero/hero-text-.js
+++ components/Hero/hero-text-.js
@@ -12,15 +12,15 @@ function HeroText() {
     return (
         <Fragment>
             {/*============= Hero-Column-2 ============= */}
-            <div class="p-12 text-shadow">
-                <div class="font-sans text-left pt-[200px] lg:pt-[300px]">
+            <div className="p-12 text-shadow">
+                <div className="font-sans text-left pt-[200px] lg:pt-[100px]">
                     {/*============= Row 1 / Title============= */}
-                    <h2 class="lg:leading-[100px] text-[30px] md:text-[60px] leading-[1.2] text-slate-700 font-serif uppercase">
+                    <h2 className="lg:leading-[100px] text-[30px] md:text-[60px] leading-[1.2] text-slate-700 font-serif uppercase">
                         Valentin Law LLC
                     </h2>
 
                     {/*============= Row 2 / Subtitle ============= */}
-                    <h3 class="text-[14px] md:text-[25px] mt-[12px] text-slate-700 font-serif uppercase">
+                    <h3 className="text-[14px] md:text-[25px] mt-[12px] text-slate-700 font-serif uppercase">
                         {t.slogan}
                     </h3>
                 </div>
