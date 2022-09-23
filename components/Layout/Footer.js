64b67745733680e31d1418ce81c690d019908361
Mauricio Chavez <mauriciochavez792@gmail.com>
2022-09-23 15:48:14 -0400

    cleaned up footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,72 +1,78 @@
-
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer class=" bg-slate-800 shadow md:px-6 md:py-8 dark:bg-gray-800">
-      <div class="container mx-auto">
+    <footer className=" bg-slate-800 shadow md:px-6 md:py-16 dark:bg-gray-800">
+      <div className="container mx-auto">
         
         {/*----------------------- Row 1 ----------------------- */}
-        <div class="lg:flex sm:items-center sm:justify-between">
+        <div className="lg:flex sm:items-center sm:justify-between">
 
-          {/*----------------------- Column 1 / Logo ----------------------- */}
-          <a href="#header" class="flex justify-center">
-            <img src="logo.png" class="mr-3 w-[100px]" alt="Flowbite Logo" />
-            <span class="self-center text-2xl text-neutral-200  font-semibold">
+          {/* Logo */}
+          <Link href="#header">
+            <span className="self-center text-2xl text-neutral-200  font-semibold cursor-pointer">
               Valentin Law LLC
             </span>
+          </Link>
+
+
+          {/* Social Media */}
+          <div  className="flex justify-center gap-x-20" >
+          <a href="https://www.instagram.com/atlimmigration/" className="text-white">
+            <i className="fa-brands fa-instagram fa-2x"/>
           </a>
 
-          <div  class="flex justify-center gap-x-20" >
-          <a href="https://www.instagram.com/atlimmigration/" class="text-white">
-            <i class="fa-brands fa-instagram fa-2x"></i>
+          <a href="https://www.youtube.com/channel/UCw9NjJpXBRKdm9ch_kj1wDQ" className="text-white">
+            <i className="fa-brands fa-youtube fa-2x"/>
           </a>
 
-          <a href="https://www.youtube.com/channel/UCw9NjJpXBRKdm9ch_kj1wDQ" class="text-white">
-            <i class="fa-brands fa-youtube fa-2x"></i>
+          <a href="https://www.tiktok.com/@immigrationatl" className="text-white">
+            <i className="fa-brands fa-tiktok fa-2x"/>
           </a>
 
-          <a href="https://www.tiktok.com/@immigrationatl" class="text-white">
-            <i class="fa-brands fa-tiktok fa-2x"></i>
-          </a>
-
-          <a href="https://twitter.com/valentinlawllc" class="text-white">
-            <i class="fa-brands fa-twitter fa-2x"></i>
+          <a href="https://twitter.com/valentinlawllc" className="text-white">
+            <i className="fa-brands fa-twitter fa-2x"/>
           </a>
 
           </div>
 
-          {/*----------------------- Column 2 ----------------------- */}
-          <ul class="flex justify-evenly items-center mb-6 text-sm text-neutral-200 py-8 ">
-            <li>
-              <a href="#about" class="mr-4 hover:underline md:mr-6 ">
+          {/* Legal */}
+          <div className="flex gap-x-4 items-center text-sm text-neutral-200">
+
+            <div>
+              <Link href="#about" >
                 About
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
+              </Link>
+            </div>
+
+            <div>
+              <Link href="#" >
                 Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
+              </Link>
+            </div>
+
+            <div>
+              <Link href="#">
                 Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#contact-us" class="hover:underline">
+              </Link>
+            </div>
+
+            <div>
+              <Link href="#contact-us">
                 Contact
-              </a>
-            </li>
-          </ul>
+              </Link>
+            </div>
+
+          </div>
         </div>
 
         {/*----------------------- Row 2 ----------------------- */}
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
         {/*----------------------- Row 3 ----------------------- */}
-        <span class="flex justify-center text-sm text-neutral-200 sm:text-center dark:text-gray-400 py-4">
+        <span className="flex justify-center text-sm text-neutral-200 sm:text-center dark:text-gray-400 py-4">
           © 2022{" "}
-          <a href="https://flowbite.com" class="hover:underline px-2">
+          <a href="https://jesuschavez.dev" className="hover:underline px-2">
             Valentin Law LLC™
           </a>
           . All Rights Reserved.
