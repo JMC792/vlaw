7fea9c5ea75b48363844567b3e8927e0fc8b5855
Mauricio Chavez <mauriciochavez792@gmail.com>
2022-11-29 22:04:28 -0500

    removed free initial consultation

--- components/Homepage/Notification.js
+++ components/Homepage/Notification.js
@@ -16,19 +16,20 @@ function Notification() {
       <section className="py-[50px] bg-back-9 ">
         <div className="container mx-auto">
           
-          {/*============= Row 1  ============= */}
+          {/* Let us put out knowledge and experience to work for you */}
           <h1 className="md:text-[40px] text-center font-serif text-neutral-200 uppercase">
             {t.notificationTitle}
           </h1>
 
-          {/*============= Row 1  ============= */}
-          <h1 className="md:text-[28px] text-center text-neutral-200 py-4">
-            {t.notificationSubtitle}
-          </h1>
-          {/*============= Row 2 / Buttons ============= */}
+          {/*/!* Free Initial Consultations *!/*/}
+          {/*<h1 className="md:text-[28px] text-center text-neutral-200 py-4">*/}
+          {/*  {t.notificationSubtitle}*/}
+          {/*</h1>*/}
+
+          {/* Buttons */}
           <div className="mt-8 md:flex grid justify-center md:justify-evenly gap-y-8">
             
-            {/*============= Button 1 ============= */}
+            {/* Button 1 */}
             <a href="https://secure.lawpay.com/pages/valentinlaw/operating">
               <button
                 type="button"
@@ -38,7 +39,7 @@ function Notification() {
               </button>
             </a>
 
-            {/*============= Button 2 ============= */}
+            {/* Button 2 */}
             <a href="https://koalendar.com/e/consultation-koaIkkTh">
               <button
                 type="button"
